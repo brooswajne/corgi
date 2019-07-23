a1c5af79bd7553d37ebba6ca9fc04aebc64f1f38
Willem Garnier <willem.garnier@luminance.com>
2019-07-23 09:37:11 +0100

    use eslintrc.js instead of yaml

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true,
+    },
+    "parserOptions": {
+        "ecmaVersion": 2019,
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        // errors
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"],
+        "no-console": "error",
+        "no-unused-vars": ["error", {
+            "args": "none",
+            "ignoreRestSiblings": true,
+        }],
+        "eol-last": ["error", "always"],
+        "dot-location": ["error", "property"],
+        "no-useless-return": "error",
+        "consistent-this": ["error", "self"],
+        "camelcase": "error",
+        "comma-dangle": ["error", "always-multiline"],
+        "brace-style": ["error", "1tbs", {
+            "allowSingleLine": true,
+        }],
+        "func-call-spacing": ["error", "never"],
+        "block-spacing": ["error", "always"],
+        "no-trailing-spaces": "error",
+        "space-before-function-paren": ["error", "never"],
+        "space-before-blocks": "error",
+        "no-var": "error",
+        "consistent-return": ["error", {
+            "treatUndefinedAsUnspecified": true,
+        }],
+        // warnings
+        "no-unneeded-ternary": "warn",
+        "no-multiple-empty-lines": "warn",
+        "keyword-spacing": "warn",
+        "no-extra-parens": ["warn", "all"],
+    }
+};
